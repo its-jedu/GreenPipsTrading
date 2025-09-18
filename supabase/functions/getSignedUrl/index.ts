d14b57458cbb3297aca8afb9ca64d0f0d2bf39d3
its_jedu <olajideweb@gmail.com>
2025-09-18 13:06:44 +0100

    Tested

--- supabase/functions/getSignedUrl/index.ts
+++ supabase/functions/getSignedUrl/index.ts
@@ -6,8 +6,8 @@ const corsHeaders = {
   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
 };
 
-const SUPABASE_URL = "https://smvxgcbylqtnncanucdk.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtdnhnY2J5bHF0bm5jYW51Y2RrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxMDgzNDksImV4cCI6MjA3MzY4NDM0OX0.mjC6vcJu1d79W6mrD_o7FkGdBkhHa3jx9Mm_bB-GUtY";
+const SUPABASE_URL = "supabase_url";
+const SUPABASE_ANON_KEY = "supabase_anon_key";
 
 serve(async (req) => {
   // Handle CORS preflight
